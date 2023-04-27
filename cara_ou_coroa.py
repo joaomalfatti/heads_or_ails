c635743b83f46756b9a2600565a17a8a3813265e
joaomalfatti <joao.cursos1210@gmail.com>
2023-04-27 14:44:05 -0400

    Foi feita uma atualização onde o usuário pode salvar os números sorteados em um arquivo, uma validação onde não pode colocar qualquer outra coisa

--- cara_ou_coroa.py
+++ cara_ou_coroa.py
@@ -1,20 +1,31 @@
 import random
 
-def cara_ou_coroa():
-    resultado = random.randint(0, 1)
-    if resultado == 0:
-        return "cara"
-    else:
-        return "coroa"
+# Perguntar ao usuário quantos números devem ser sorteados
+num_sorteados = int(input("Quantos números deseja sortear? "))
 
-escolha = ""
-while escolha not in ["cara", "coroa"]:
-    escolha = input("Escolha cara ou coroa: ")
-    escolha = escolha.lower()
+# Perguntar ao usuário os limites do intervalo de sorteio
+limite_inf = int(input("Qual o valor mínimo do intervalo? "))
+limite_sup = int(input("Qual o valor máximo do intervalo? "))
 
-resultado = cara_ou_coroa()
+# Sortear os números aleatórios
+sorteados = []
+for i in range(num_sorteados):
+    sorteados.append(random.randint(limite_inf, limite_sup))
 
-if escolha == resultado:
-    print("Você ganhou!")
+# Exibir os números sorteados
+print(f"Os {num_sorteados} números sorteados entre {limite_inf} e {limite_sup} foram:")
+for i in sorteados:
+    print(i)
+
+# Perguntar ao usuário se deseja salvar os números sorteados em um arquivo
+salvar_arquivo = input("Deseja salvar os números sorteados em um arquivo? (s/n) ").lower()
+if salvar_arquivo == "s":
+    nome_arquivo = input("Digite o nome do arquivo: ")
+    with open(nome_arquivo, "w") as arquivo:
+        for i in sorteados:
+            arquivo.write(str(i) + "\n")
+    print("Números sorteados salvos com sucesso!")
+elif salvar_arquivo == "n":
+    print("Os números sorteados não foram salvos em um arquivo.")
 else:
-    print("Você perdeu!")
+    print("Opção inválida. Os números sorteados não foram salvos em um arquivo.")
